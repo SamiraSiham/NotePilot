decd27c5d883d4d1df8a802e410418e01095c944
Samira Siham <88947861+SamiraSiham@users.noreply.github.com>
2025-08-12 00:01:38 +0100

    footer

--- src/app/components/CallToAction.jsx
+++ src/app/components/CallToAction.jsx
@@ -7,7 +7,7 @@ export const CallToAction = () => {
           No learning curve. No subscription headaches. Just install and start grading.
         </p>
         <form className="mt-10 flex flex-col gap-2.5 max-w-sm mx-auto sm:flex-row">
-          <input type="email" name="" placeholder="your@email.com" id="" className="h-12 bg-white/20 rounded-lg px-5 py-3 font-medium placeholder:text-[#9CA3AF] flex-1"/>
+          <input type="email" name="" placeholder="your@email.com" id="" className="h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3AF] sm:flex-1"/>
           <button className="bg-white text-black h-12 rounded-lg px-5">Get Access</button>
         </form>
       </div>
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,3 +1,21 @@
+import InstaIcon from '../assets/icons/insta.svg';
+import XIcon from '../assets/icons/x-social.svg';
+import TikTokIcon from '../assets/icons/tiktok.svg';
+import YoutubeIcon from '../assets/icons/youtube.svg';
 export const Footer = () => {
-  return null;
+  return (
+    <footer className='py-5 bg-black text-white/60 border-t border-white/20'>
+      <div className="container">
+        <div className="flex flex-col gap-4 sm:flex-row sm:justify-between">
+          <div className="text-center">© 2025 Samira SIHAM. All rights reserved</div>
+          <ul className="flex justify-center gap-2.5">
+            <li><XIcon/></li>
+            <li><InstaIcon/></li>
+            <li><TikTokIcon/></li>
+            <li><YoutubeIcon/></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
