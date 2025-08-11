1c60277f36ce151df195cdfafb86136109c35d56
Samira Siham <88947861+SamiraSiham@users.noreply.github.com>
2025-08-11 19:12:36 +0100

    FAQ

--- src/app/components/FAQs.jsx
+++ src/app/components/FAQs.jsx
@@ -1,26 +1,59 @@
+"use client"
+import { useState } from 'react';
+import PlusIcon from '../assets/icons/plus.svg';
+import MinusIcon from '../assets/icons/minus.svg';
+import clsx from 'clsx';
 const items = [
   {
-    question: "What payment methods do you accept?",
+    question: "Is the app free to use?",
     answer:
-      "We accept all major credit cards, PayPal, and various other payment methods depending on your location. Please contact our support team for more information on accepted payment methods in your region.",
+      "We offer a free trial so you can test all features. A one-time license is available for unlimited use.",
   },
   {
-    question: "How does the pricing work for teams?",
+    question: "Can I import my existing grades?",
     answer:
-      "Our pricing is per user, per month. This means you only pay for the number of team members you have on your account. Discounts are available for larger teams and annual subscriptions.",
-  },
-  {
-    question: "Can I change my plan later?",
-    answer:
-      "Yes, you can upgrade or downgrade your plan at any time. Changes to your plan will be prorated and reflected in your next billing cycle.",
+      "Yes. You can import from Excel or CSV files in just a few clicks.",
   },
   {
     question: "Is my data secure?",
     answer:
-      "Security is our top priority. We use state-of-the-art encryption and comply with the best industry practices to ensure that your data is stored securely and accessed only by authorized users.",
+      "Absolutely. Your data is stored locally and never shared without your permission.",
+  },
+  {
+    question: "Does it support different grading systems?",
+    answer:
+      "Yes. You can customize grading scales and criteria to match your institution’s requirements.",
   },
 ];
-
+const AccordionItem = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div key={question} className='py-7 border-b border-white/30' onClick={()=>setIsOpen(!isOpen)}>
+      <div className='flex items-center'>
+        <span className='flex-1 text-lg font-bold'>
+          {question}
+        </span>
+        {isOpen ? <MinusIcon/> : <PlusIcon />}
+        
+      </div>
+      <div className={clsx("mt-4",{
+        'hidden': !isOpen,
+        '': isOpen === true
+      })}>{answer}</div>
+    </div>
+  )
+}
 export const FAQs = () => {
-  return null;
+  return (
+    <div className="bg-black text-white bg-gradient-to-b from-primary to-black py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className='text-center text-5xl sm:text-6xl sm:max-w-[648px] mx-auto font-bold tracking-tighter'>Frequently Asked Questions</h2>
+        <div className='mt-12 max-w-[648px] mx-auto'>
+          {items.map(({ question, answer }) => (
+            <AccordionItem key={question} question={question} answer={answer}/>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
