8fb63a75933342c20d5c435fcde0f04ed349266e
Samira Siham <88947861+SamiraSiham@users.noreply.github.com>
2025-08-11 23:46:54 +0100

    call to action

--- src/app/components/CallToAction.jsx
+++ src/app/components/CallToAction.jsx
@@ -1,3 +1,16 @@
 export const CallToAction = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24 text-center">
+      <div className="container max-w-xl">
+        <h2 className="font-bold text-5xl tracking-tighter sm:text-6xl">Start Managing Grades Smarter — Today</h2>
+        <p className="text-xl text-white/70 mt-5">
+          No learning curve. No subscription headaches. Just install and start grading.
+        </p>
+        <form className="mt-10 flex flex-col gap-2.5 max-w-sm mx-auto sm:flex-row">
+          <input type="email" name="" placeholder="your@email.com" id="" className="h-12 bg-white/20 rounded-lg px-5 py-3 font-medium placeholder:text-[#9CA3AF] flex-1"/>
+          <button className="bg-white text-black h-12 rounded-lg px-5">Get Access</button>
+        </form>
+      </div>
+    </div>
+  );
 };
